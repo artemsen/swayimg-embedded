f188fcd1d9199ea18bc0d3614cddfada70b8d35f
Artem Senichev <artemsen@gmail.com>
2026-03-22 15:06:22 +0300

    Fix config
    Signed-off-by: Artem Senichev <artemsen@gmail.com>

--- swayimg/package/swayimg/swayimg.mk
+++ swayimg/package/swayimg/swayimg.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SWAYIMG_VERSION = ef2babde13786a538f9a4e3b6909f4922ed44990
+SWAYIMG_VERSION = 3c43ce3a58c85c1da45907ef555664737ccc2f81
 SWAYIMG_SITE = $(call github,artemsen,swayimg,$(SWAYIMG_VERSION))
 SWAYIMG_LICENSE = MIT
 SWAYIMG_LICENSE_FILES = LICENSE
